adfeb0de03c4b0cfea6a6a9c71a3b6b92c0a254f
Vashkevich Yuri <vashkevichyp@gmail.com>
2021-11-23 01:10:36 +0300

    init: start crud-task, init config webpack

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# simple-crud-api
+# Simple-crud-api
+
+**Usage:**
+
+- Before run app you need install: `npm install`
+
+- Run app in development mode: `npm run dev`
+- Run app in development mode this watch: `npm run start`
+- Run app in prodaction mode: `npm run start:prod`
+
+- Run test: `npm run test`
+- Run test:coverage: `npm run test:coverage`
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const path = require('path');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const config = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [new CleanWebpackPlugin()],
+  module: {},
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};
